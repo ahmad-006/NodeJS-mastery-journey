f7b058cc10ab79e8ea99a8b63f52ea2aad726c76
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-12-10 00:18:19 +0500

    feat: took a look at some methods of request to understand it better

--- 03-basics/app.js
+++ 03-basics/app.js
@@ -4,7 +4,8 @@ import http from "http";
 //Creating a server with http.createServer which takes callbck finction with parameters of request and response to access some detailed insights of request received and response sent
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // taking a look at few of the request methods to understand it better.....
+  console.log(req.url, req.method, req.headers);
 });
 
 //listening to the server created above so that runtime keep awake to listen to every request made to the server so that we can send response to each request
