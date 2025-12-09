e1378128ce0a1c4dc40a0f5a04af487ecc992e7f
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-12-10 00:45:52 +0500

    feat: sent the response back to client after request

--- 03-basics/app.js
+++ 03-basics/app.js
@@ -6,9 +6,22 @@ import http from "http";
 const server = http.createServer((req, res) => {
   // taking a look at few of the request methods to understand it better.....
   console.log(req.url, req.method, req.headers);
+
+  //setting header and sending response to understand how server side responses work....
+
+  //set header
+  res.setHeader("content-type", "text/html");
+
+  //sending response in html
+  res.write(
+    "<html><head><title>My Server</title></head><body><h1>Welcome to my Page </h1></body></html>"
+  );
+
+  //res.end() ends the response and before it is what is seen by client after it any response is not shown to client
+  res.end();
 });
 
 //listening to the server created above so that runtime keep awake to listen to every request made to the server so that we can send response to each request
 
 //passing the port to which it listens for request
-server.listen(3000);
+server.listen(3002);
