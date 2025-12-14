149283f8cf5e588e802a92e4a0ccf8f3a7d1fdc7
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-12-14 21:16:17 +0500

    feat:implemented first middleware in expressJS

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ import express from "express";
 
 const app = express();
 
+//?adding a middleware
+app.use((req, res, next) => {
+  console.log("Hello from the middleware...!");
+
+  //? next() allows program to move to next middleware
+  next();
+});
+
 // 'app' is just a function with the signature (req, res, next).
 // We can pass it to the raw Node HTTP server to handle all incoming requests.
 const server = http.createServer(app);
