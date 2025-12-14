bd40e6a8be44cda238404a2a0d7fbf899c9ed59f
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-12-15 00:38:28 +0500

    feat: implemented server listen using express app function

--- 05-express/app.js
+++ 05-express/app.js
@@ -20,9 +20,14 @@ app.use((req, res, next) => {
   res.send("<h1>Hello from the ExpressJS......</h1>");
 });
 
+//*         Semi - Vanilla Way
 // 'app' is just a function with the signature (req, res, next).
 // We can pass it to the raw Node HTTP server to handle all incoming requests.
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-// Starts the server process and keeps the Event Loop running on port 3000.
-server.listen(3000);
+// //? Starts the server process and keeps the Event Loop running on port 3000.
+// server.listen(3000);
+
+//*         Express Way
+//? using express's app function to listen to 3000 port
+app.listen(3000);
