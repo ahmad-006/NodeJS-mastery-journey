8e4fd47ee24704eaf06b1929df0fc977b3f2c550
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-12-14 21:15:10 +0500

    feat: created a server through express

---
+++ app.js
@@ -0,0 +1,14 @@
+//importing nodejs core modules
+import http from "http";
+
+// Importing express
+import express from "express";
+
+const app = express();
+
+// 'app' is just a function with the signature (req, res, next).
+// We can pass it to the raw Node HTTP server to handle all incoming requests.
+const server = http.createServer(app);
+
+// Starts the server process and keeps the Event Loop running on port 3000.
+server.listen(3000);
