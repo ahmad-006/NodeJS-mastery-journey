1dd5be9e2b931067f3f578d34b4934c885ca2606
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-12-17 20:12:18 +0500

    refactor(express): minor cleanup in admin routes

--- 05-express/routes/admin.js
+++ 05-express/routes/admin.js
@@ -1,6 +1,7 @@
 import path from "path";
 
 import express from "express";
+import { products } from "06-templating/routes/admin.js";
 const router = express.Router();
 
 //? add-product route
@@ -11,6 +12,7 @@ router.get("/add-product", (req, res, next) => {
 //? implementing /product route
 // Using app.post so that it only receives post requests
 router.post("/product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
