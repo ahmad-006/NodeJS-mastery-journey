efe362b33c650e39bbb8d3a188f23ea18619c00a
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-01-10 00:49:42 +0500

    Refactor: Fmerged everything to main

--- 07- MVC/routes/shop.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getProducts } from "../../07- MVC/controllers/products.js";
-
-const router = express.Router();
-
-//? /  (default) route
-router.get("/", getProducts);
-
-export default router;
