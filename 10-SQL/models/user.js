d96b36299c02fbdc0dae4684d33b6e98098882f1
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-01-25 17:49:29 +0500

    feat(10-SQL): Add User model

---
+++ 10-SQL/models/user.js
@@ -0,0 +1,13 @@
+import { sequelize } from "../util/database.js";
+import { DataTypes } from "sequelize/types/index.js";
+
+export const User = sequelize.define("user", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: DataTypes.STRING,
+  email: DataTypes.STRING,
+});
