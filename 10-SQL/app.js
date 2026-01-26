cdac3c1130bb1748fda5a8cfc993838e166c3b7e
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-01-26 20:42:16 +0500

    Feat: Initialize app with User-Cart associations and Sequelize sync

--- 10-SQL/app.js
+++ 10-SQL/app.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 // importing core modules
 import path from "path";
 
@@ -9,6 +10,10 @@ import { router as adminRoutes } from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
 import { pageNotFound } from "./controllers/404.js";
 import { sequelize } from "./util/database.js";
+import { Product } from "./models/product.js";
+import { User } from "./models/user.js";
+import { Cart } from "./models/cart.js";
+import { CartItem } from "./models/cart-item.js";
 
 const app = express();
 
@@ -22,15 +27,55 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 //? middlewares for routes
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findByPk(1);
+    if (!user) {
+      return next();
+    }
+    // @ts-ignore
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error("Auth Middleware Error:", err);
+    next(err);
+  }
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(pageNotFound);
 
 //? connecting DB with the server
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Product.belongsToMany(Cart, { through: CartItem });
+Cart.belongsToMany(Product, { through: CartItem });
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((result) => {
-    console.log("DB connected sucs\cessfully");
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Ahmad", email: "MrSheikho@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    return user.getCart().then((cart) => {
+      if (!cart) {
+        return user.createCart();
+      }
+      return cart;
+    });
+  })
+  .then((cart) => {
+    console.log("DB connected successfully");
+
     //? using express's app function to listen to 3000 port if DB is connected successfully
     app.listen(3000);
   })
