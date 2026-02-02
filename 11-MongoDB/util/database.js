1e641caac77477d58ee07c271874fae10b56c7cd
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-02 18:44:26 +0500

    Add MongoDB connection utility and dependencies

--- 11-MongoDB/util/database.js
+++ 11-MongoDB/util/database.js
@@ -1,11 +1,21 @@
-import { Sequelize } from "sequelize";
+import mongodb from "mongodb";
 
-export const sequelize = new Sequelize(
-  "node-complete",
-  "root",
-  "MrSheikho1806",
-  {
-    dialect: "mysql",
-    host: "localhost",
-  },
-);
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+export const mongoConnect = (cb) => {
+  MongoClient.connect(
+    "mongodb+srv://mrsheikho-06:RK5SOUbYuNnsPIM7@cluster0.cv16iwb.mongodb.net/shop?appName=Cluster0",
+  )
+    .then((client) => {
+      _db = client.db("shop");
+      cb(client);
+    })
+    .catch((err) => console.error(err));
+};
+
+export const getDb = () => {
+  if (_db) return _db;
+  console.log("No DB found");
+};
