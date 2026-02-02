17ba14a611cfdf19911dcbf5fe0cca7acfaefb6c
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-02 18:45:27 +0500

    Implement Product model with MongoDB

--- 11-MongoDB/models/product.js
+++ 11-MongoDB/models/product.js
@@ -1,34 +1,64 @@
-import { sequelize } from "../util/database.js";
-import { DataTypes } from "sequelize";
+import { getDb } from "../util/database.js";
+import mongodb from "mongodb";
 
-export const Product = sequelize.define("product", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  price: {
-    type: DataTypes.DOUBLE,
-    allowNull: false,
-  },
-  imageUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "users",
-      key: "id",
-    },
-  },
-});
+export class Product {
+  constructor(title, price, imageUrl, description, _id, userId) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this._id = _id;
+    this.userId = userId;
+  }
+
+  save() {
+    const db = getDb();
+    db.collection("products")
+      .insertOne(this)
+      .then(() => {
+        console.log("Product added .....!");
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }
+  static fetchAll(cb) {
+    const db = getDb();
+    return db.collection("products").find().toArray();
+  }
+
+  static updateById(id, updated) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .updateOne({ _id: new mongodb.ObjectId(id) }, { $set: updated })
+      .then(() => {
+        console.log("Product updated....!");
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }
+
+  static findById(id) {
+    const db = getDb();
+
+    return db
+      .collection("products")
+      .find({ _id: new mongodb.ObjectId(id) })
+      .next()
+      .then((product) => {
+        console.log(product);
+        return product;
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }
+  static deleteById(id) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .deleteOne({ _id: new mongodb.ObjectId(id) });
+  }
+}
