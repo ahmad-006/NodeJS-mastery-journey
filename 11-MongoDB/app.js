d8feacee3ed9595ef3b429c1f62cc605e05c2d47
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-02 18:45:11 +0500

    Refactor app entry point to use MongoDB connection

--- 11-MongoDB/app.js
+++ 11-MongoDB/app.js
@@ -9,13 +9,10 @@ import express from "express";
 import { router as adminRoutes } from "./routes/admin.js";
 import shopRoutes from "./routes/shop.js";
 import { pageNotFound } from "./controllers/404.js";
-import { sequelize } from "./util/database.js";
-import { Product } from "./models/product.js";
+
+//import db connection
+import { mongoConnect } from "./util/database.js";
 import { User } from "./models/user.js";
-import { Cart } from "./models/cart.js";
-import { CartItem } from "./models/cart-item.js";
-import { Order } from "./models/order.js";
-import { OrderItem } from "./models/order-item.js";
 
 const app = express();
 
@@ -29,62 +26,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 //? middlewares for routes
-app.use(async (req, res, next) => {
-  try {
-    const user = await User.findByPk(1);
-    if (!user) {
-      return next();
-    }
-    // @ts-ignore
-    req.user = user;
-    next();
-  } catch (err) {
-    console.error("Auth Middleware Error:", err);
-    next(err);
-  }
+
+app.use((req, res, next) => {
+  User.findById("697ca8657dd4dd5d6a6eadb7")
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
+      next();
+    })
+    .catch((err) => console.log(err));
 });
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 app.use(pageNotFound);
 
-//? connecting DB with the server
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-Product.belongsToMany(Cart, { through: CartItem });
-Cart.belongsToMany(Product, { through: CartItem });
-Order.belongsTo(User);
-User.hasMany(Order);
-Order.belongsToMany(Product, { through: OrderItem });
-Product.belongsToMany(Order, { through: OrderItem });
-
-sequelize
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Ahmad", email: "MrSheikho@gmail.com" });
-    }
-    return user;
-  })
-  .then((user) => {
-    return user.getCart().then((cart) => {
-      if (!cart) {
-        return user.createCart();
-      }
-      return cart;
-    });
-  })
-  .then((cart) => {
-    console.log("DB connected successfully");
-
-    //? using express's app function to listen to 3000 port if DB is connected successfully
-    app.listen(3000);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+mongoConnect((result) => {
+  app.listen(3000);
+});
