d0d1e85ceb9460b2490abe740f7024c8d4aea60f
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-07 09:00:00 +0500

    refactor(shop): Migrate controller logic to mongoose

--- 12-Mongoose/controllers/shop.js
+++ 12-Mongoose/controllers/shop.js
@@ -1,22 +1,21 @@
+import { Order } from "../models/order.js";
 import { Product } from "../models/product.js";
 
-const getIndex = (req, res) => {
-  Product.fetchAll().then((products) => {
-    res.render("shop/index", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "/",
-    });
+const getIndex = async (req, res) => {
+  const products = await Product.find();
+  res.render("shop/index", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
   });
 };
 
-const getProducts = (req, res) => {
-  Product.fetchAll().then((products) => {
-    res.render("shop/products-list", {
-      prods: products,
-      pageTitle: "All Products",
-      path: "/products",
-    });
+const getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.render("shop/products-list", {
+    prods: products,
+    pageTitle: "All Products",
+    path: "/products",
   });
 };
 
@@ -33,14 +32,18 @@ const getProductId = (req, res, next) => {
     .catch((err) => console.log(err.sqlMessage));
 };
 
-const getCart = (req, res) => {
+const getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then((products) => {
+    .populate("cart.items.productId")
+    .then((user) => {
+      // Filter out items where the associated product no longer exists
+      const products = user.cart.items.filter((item) => item.productId);
+
       let totalPrice = 0;
       products.forEach((p) => {
-        totalPrice += p.price * p.quantity;
+        totalPrice += p.productId.price * p.quantity;
       });
+
       res.render("shop/cart", {
         path: "/cart",
         pageTitle: "Your Cart",
@@ -50,14 +53,16 @@ const getCart = (req, res) => {
     })
     .catch((err) => console.error(err));
 };
-const postCart = (req, res, next) => {
-  const { productId: id } = req.body;
 
-  Product.findById(id)
-    .then((product) => {
-      return req.user.addToCart(product);
-    })
-    .then((result) => res.redirect("/cart"));
+const postCart = async (req, res, next) => {
+  const { productId: id } = req.body;
+  try {
+    const product = await Product.findById(id);
+    await req.user.addToCart(product);
+    res.redirect("/cart");
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 const postDeleteCartProduct = async (req, res, next) => {
@@ -71,23 +76,39 @@ const getCheckout = (req, res) => {
     pageTitle: "Checkout",
   });
 };
-const getOrders = (req, res) => {
-  req.user
-    .getOrder()
-    .then((orders) => {
-      res.render("shop/orders", {
-        path: "/orders",
-        pageTitle: "Your Orders",
-        orders,
-      });
-    })
-    .catch((err) => console.error(err));
+const getOrders = async (req, res) => {
+  const orders = await Order.find({ "user.userId": req.user._id });
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+    orders,
+  });
 };
-const postOrders = (req, res, next) => {
-  req.user
-    .addOrder()
-    .then(() => res.redirect("/orders"))
-    .catch((err) => console.error(err));
+const postOrders = async (req, res, next) => {
+  try {
+    const user = await req.user.populate("cart.items.productId");
+    // updating products to required form
+    const products = user.cart.items.map((p) => {
+      return { product: { ...p.productId._doc }, quantity: p.quantity };
+    });
+
+    //creating a new order
+    const newOrder = await new Order({
+      products,
+      user: {
+        name: req.user.name,
+        userId: req.user._id,
+      },
+    });
+    //saving the order
+    await newOrder.save();
+
+    //clearing the cart
+    req.user.cart.items = [];
+    await req.user.save();
+
+    res.redirect("/orders");
+  } catch (error) {}
 };
 
 export {
