e2e67e22fb6feae9c091cac79a3de94051bda602
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-09 09:10:00 +0500

    feat: implement login/logout logic and session storage

--- 13-Auth-cookies/controllers/auth.js
+++ 13-Auth-cookies/controllers/auth.js
@@ -1,6 +1,25 @@
+import { User } from "../models/user.js";
+
 export const getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+  });
+};
+
+export const postLogin = async (req, res, next) => {
+  const user = await User.findById("6980c48cf45b79db1d3a3033");
+  req.session.isLoggedIn = true;
+  req.session.user = user;
+  req.session.save((err) => {
+    if (err) console.log(err);
+    res.redirect("/");
+  });
+};
+
+export const postLogout = (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) console.log(err);
+    res.redirect("/");
   });
 };
