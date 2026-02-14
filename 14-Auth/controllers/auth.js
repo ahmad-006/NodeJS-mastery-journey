7e1b3a55f5c2d7026bdc8e2b15b605a2dad8b08e
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-14 16:33:10 +0500

    feat: implement flash messages in auth controller

--- 14-Auth/controllers/auth.js
+++ 14-Auth/controllers/auth.js
@@ -2,18 +2,32 @@ import { User } from "../models/user.js";
 import bcrypt from "bcryptjs";
 
 export const getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
     isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
 export const getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
     isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -21,27 +35,45 @@ export const postLogin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const user = await User.findOne({ email });
-    if (!user) return res.redirect("/login");
+    if (!user) {
+      req.flash("error", "Invalid email or password.");
+      return res.redirect("/login");
+    }
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
-    if (!isPasswordCorrect) return res.redirect("/login");
+    if (!isPasswordCorrect) {
+      req.flash("error", "Invalid email or password.");
+      return res.redirect("/login");
+    }
     req.session.isLoggedIn = true;
     req.session.user = user;
     req.session.save((err) => {
       console.log(err);
       res.redirect("/");
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const postSignup = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const user = await User.findOne({ email });
-  if (!!user) return res.redirect("/singup");
-  const hashedPassword = await bcrypt.hash(password, 12);
-  const newUser = new User({ email, password: hashedPassword });
-  await newUser.save();
-  res.redirect("/login");
+  try {
+    const user = await User.findOne({ email });
+    if (!!user) {
+      req.flash(
+        "error",
+        "E-Mail exists already, please pick a different one."
+      );
+      return res.redirect("/signup");
+    }
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ email, password: hashedPassword });
+    await newUser.save();
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const postLogout = (req, res, next) => {
