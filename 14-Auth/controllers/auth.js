1d730d1cad47310375cfe16f6c735365e7fbe86a
Ahmad Aamir <ahmadaamir1806@gmail.com>
2026-02-15 13:00:00 +0500

    Feat: Implement 'request password reset' functionality (includes mailer setup and crypto import)

--- 14-Auth/controllers/auth.js
+++ 14-Auth/controllers/auth.js
@@ -1,6 +1,15 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcryptjs";
+import mailer from "nodemailer";
+import crypto from "crypto";
 
+const transporter = mailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD,
+  },
+});
 export const getLogin = (req, res, next) => {
   let message = req.flash("error");
   if (message.length > 0) {
@@ -11,7 +20,6 @@ export const getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    isAuthenticated: false,
     errorMessage: message,
   });
 };
@@ -23,10 +31,10 @@ export const getSignup = (req, res, next) => {
   } else {
     message = null;
   }
+
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
-    isAuthenticated: false,
     errorMessage: message,
   });
 };
@@ -55,21 +63,128 @@ export const postLogin = async (req, res, next) => {
   }
 };
 
+export const getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset Password",
+    errorMessage: message,
+  });
+};
+
+export const postReset = async (req, res, next) => {
+  try {
+    const buffer = await crypto.randomBytes(32);
+    const token = buffer.toString("hex");
+    const user = await User.findOne({ email: req.body.email });
+
+    if (!user) {
+      req.flash("error", "No account with that email found.");
+      return res.redirect("/reset");
+    }
+
+    user.resetToken = token;
+    user.resetTokenExpiration = Date.now() + 3600000; // 1 hour from now
+    await user.save();
+
+    res.redirect("/");
+    await transporter.sendMail({
+      to: req.body.email,
+      from: "financybuddy@gmail.com",
+      subject: "Password Reset",
+      html: `
+        <p>You requested a password reset</p>
+        <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+      `,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getNewPassword = async (req, res, next) => {
+  const token = req.params.token;
+  try {
+    const user = await User.findOne({
+      resetToken: token,
+      resetTokenExpiration: { $gt: Date.now() },
+    });
+    if (!user) {
+      req.flash("error", "Password reset token is invalid or has expired.");
+      return res.redirect("/reset");
+    }
+
+    let message = req.flash("error");
+    if (message.length > 0) {
+      message = message[0];
+    } else {
+      message = null;
+    }
+    res.render("auth/new-password", {
+      path: "/new-password",
+      pageTitle: "New Password",
+      errorMessage: message,
+      userId: user._id.toString(),
+      passwordToken: token,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const postNewPassword = async (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  try {
+    const user = await User.findOne({
+      resetToken: passwordToken,
+      resetTokenExpiration: { $gt: Date.now() },
+      _id: userId,
+    });
+
+    if (!user) {
+      req.flash("error", "Password reset token is invalid or has expired.");
+      return res.redirect("/reset");
+    }
+
+    resetUser = user;
+    const hashedPassword = await bcrypt.hash(newPassword, 12);
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = undefined;
+    resetUser.resetTokenExpiration = undefined;
+    await resetUser.save();
+    res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const postSignup = async (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   try {
     const user = await User.findOne({ email });
     if (!!user) {
-      req.flash(
-        "error",
-        "E-Mail exists already, please pick a different one."
-      );
+      req.flash("error", "E-Mail exists already, please pick a different one.");
       return res.redirect("/signup");
     }
     const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
+    await transporter.sendMail({
+      to: email,
+      from: "financybuddy@gmail.com",
+      subject: "Signup succeeded!",
+      html: "<h1>You successfully signed up!</h1>",
+    });
     res.redirect("/login");
   } catch (error) {
     console.log(error);
